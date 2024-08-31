1f5ec5e23faa2d9566620627a30e607feb484d62
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-08-31 14:41:13 +0800

    Added image icon

--- info_collector.java
+++ info_collector.java
@@ -1,19 +1,23 @@
+import java.awt.Image;
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class info_collector {
     public static void main(String[] args) {
         
-        String ID_Number = JOptionPane.showInputDialog("Enter ID Number: ");
+        ImageIcon originalIcon = new ImageIcon("information_icon.png");
+        Image resizedImage     = originalIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        ImageIcon icon         = new ImageIcon(resizedImage);
 
-        String name = JOptionPane.showInputDialog("Enter Full Name: ");
+        String ID_Number = JOptionPane.showInputDialog(null, "Enter ID Number:", "User Information", JOptionPane.PLAIN_MESSAGE, icon, null, "").toString();
+        String name      = JOptionPane.showInputDialog(null, "Enter Full Name:", "User Information", JOptionPane.PLAIN_MESSAGE, icon, null, "").toString();
+        int age          = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter Age:", "User Information", JOptionPane.PLAIN_MESSAGE, icon, null, "").toString());
+        double height    = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter Height in Centimeters:", "User Information", JOptionPane.PLAIN_MESSAGE, icon, null, "").toString());
+        double weight    = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter Weight in Kilograms:", "User Information", JOptionPane.PLAIN_MESSAGE, icon, null, "").toString());
 
-        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter Age: "));
+        String message   = String.format("Summary of your information:\nID Number: %s\nName: %s\nAge: %d\nHeight: %.2f cm\nWeight: %.2f kg", 
+                                        ID_Number, name, age, height, weight);
 
-        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter Height in Centimeters: "));
-
-        double weight = Double.parseDouble(JOptionPane.showInputDialog("Enter Weight in Kilograms: "));
-
-        String message = String.format("Summary of your information:\nName: %s\nAge: %d\nHeight: %.2f cm\nWeight: %.2f kg", name, age, height, weight);
-        JOptionPane.showMessageDialog(null, message);
+        JOptionPane.showMessageDialog(null, message, "Summary", JOptionPane.INFORMATION_MESSAGE, icon);
     }
 }
